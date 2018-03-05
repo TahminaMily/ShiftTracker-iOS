eb84443bcd998b57a0c80657226aa4bcf6625bab
Tahmina Khanam <tahmina.mily@gmail.com>
2018-03-05 23:44:20 +1100

    implement offline storage

--- ShiftTracker/Networking/DataSource.swift
+++ ShiftTracker/Networking/DataSource.swift
@@ -8,25 +8,44 @@
 
 import Foundation
 import Alamofire
+import Cache
 
 class DataSource {
-    func fetchBusiness(completion: @escaping (Result<Business>) -> Void) {
+    let storage = try? Storage(diskConfig: DiskConfig(name: "Disk"))
+
+    func fetchBusiness(completion: @escaping (Alamofire.Result<Business>) -> Void) {
         APIRouter.sessionManager
             .request(APIRouter.business)
-            .responseObject { (response: DataResponse<Business>) in
-                DispatchQueue.main.async { completion(response.result) }
+            .responseObject { [weak self] (response: DataResponse<Business>) in
+                //store in cache if successfull
+                var result = response.result
+                switch response.result {
+                case .success(let business):
+                    try? self?.storage?.setObject(business, forKey: "business")
+                case .failure:
+                    if let business:Business = (try? self?.storage?.object(ofType: Business.self, forKey: "business"))?.flatMap({ $0 }) {
+                        result = .success(business)
+                    }
+                }
+                DispatchQueue.main.async { completion(result) }
             }
     }
     
-    func fetchShifts(completion: @escaping (Result<[Shift]>) -> Void) {
+    func fetchShifts(completion: @escaping (Alamofire.Result<[Shift]>) -> Void) {
         APIRouter.sessionManager
             .request(APIRouter.shifts)
-            .responseObject { (response: DataResponse<[Shift]>) in
-                DispatchQueue.main.async { completion(response.result) }
+            .responseObject { [weak self] (response: DataResponse<[Shift]>) in
+                //store in cache if successfull
+                var result = response.result
+                switch response.result {
+                case .success(let shifts):
+                    try? self?.storage?.setObject(shifts, forKey: "shifts")
+                case .failure:
+                    if let shifts: [Shift] = (try? self?.storage?.object(ofType: [Shift].self, forKey: "shifts"))?.flatMap({ $0 }) {
+                        result = .success(shifts)
+                    }
+                }
+                DispatchQueue.main.async { completion(result) }
             }
-    }
-
-    func startShift(at: Shift.Event, completion: (Result<Void>) -> Void) {
-
     }
 }
